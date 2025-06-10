ee7e9053bae4da57811debee26e1935118849306
[peeryo] <peeryo@sfusd.edu>
2025-06-09 21:39:20 -0700

    VIdeo 2 done Player movement

--- fists-of-fury/Scenes/Characters/character.gd
+++ fists-of-fury/Scenes/Characters/character.gd
@@ -1,9 +1,41 @@
 extends CharacterBody2D
-@export var health : int
 @export var damage : int
+@export var health : int
 @export var speed : float
 
-func _process(delta: float) -> void:
+@onready var animation_player := $AnimationPlayer
+@onready var character_sprite := $CharacterSprite
+
+enum State {IDLE, WALK}
+
+var state = State.IDLE
+
+func _process(_delta: float) -> void:
+	handle_input()
+	handle_movement()
+	handle_animations()
+	flip_sprites()
+	move_and_slide()
+	
+func handle_movement() -> void:
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.WALK
+		
+func handle_input():
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
-	move_and_slide()
+	
+func handle_animations() -> void:
+	if state == State.IDLE:
+		animation_player.play("idle")
+	if state == State.WALK:
+		animation_player.play("walk")
+		
+func flip_sprites() -> void:
+	if velocity.x > 0:
+		character_sprite.flip_h = false
+	if velocity.x < 0:
+		character_sprite.flip_h = true
+	
