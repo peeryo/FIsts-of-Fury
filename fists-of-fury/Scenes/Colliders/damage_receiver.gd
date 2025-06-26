1fd28e0e9c078115a5078e37019ff13845f25129
[peeryo] <peeryo@sfusd.edu>
2025-06-26 15:21:54 -0700

    Video 5 completed
    Enemies overlapped and needed to instroduce a hotel res style system to see if spots were open, if yes, new enemy fills it, if not, extra enemy stands down, also used a few callables.

--- fists-of-fury/Scenes/Colliders/damage_receiver.gd
+++ fists-of-fury/Scenes/Colliders/damage_receiver.gd
@@ -1,4 +1,5 @@
 class_name DamageReceiver
 extends Area2D
+enum HitType {NORMAL, KNOCKDOWN, POWER}
 
-signal damage_received(damage: int, direction: Vector2)
+signal damage_received(damage: int, direction: Vector2, hit_type: HitType)
--- fists-of-fury/Scenes/Props/barrel.gd
+++ fists-of-fury/Scenes/Props/barrel.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	sprite.position = Vector2.UP * height
 	handle_air_time(delta)
 
-func on_receive_damage(damage : int, direction: Vector2) -> void:
+func on_receive_damage(_damage : int, direction: Vector2, hit_type: DamageReceiver.HitType) -> void:
 	if state == State.IDLE:
 		sprite.frame = 1
 		height_speed = knockback_intensity * 2
